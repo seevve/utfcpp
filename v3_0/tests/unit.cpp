1b439ddb7e74880f9e130e8755510ece6bb04c8e
ntrifunovic <ntrifunovic@a809a056-fc17-0410-9590-b4f493f8b08e>
2013-03-09 20:51:50 +0000

    Introducing Boost Test for unit-testing v3.

--- v3_0/tests/unit.cpp
+++ v3_0/tests/unit.cpp
@@ -1,20 +1,19 @@
-#include <assert.h>
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE UTF8_CPP_UNIT
+#include <boost/test/unit_test.hpp>
+
 #include "../src/utf8.h"
 using namespace std;
 
-int main()
+BOOST_AUTO_TEST_CASE(append)
 {
-// append
-    {
 	string s;
-	utf8::append(U'\U00000448', s);
-        assert (s.length() == 2 && s[0] == '\xd1' && s[1] == '\x88');
+    BOOST_CHECK_NO_THROW (utf8::append(U'\U00000448', s));
+	BOOST_CHECK (s.length() == 2 && s[0] == '\xd1' && s[1] == '\x88');
 
 	s.erase();
-	utf8::append(U'\U000065e5', s);
-        assert (s.length() == 3 && s[0] == '\xe6' && s[1] == '\x97' && s[2] == '\xa5');
-    }
-    
+	BOOST_CHECK_NO_THROW(utf8::append(U'\U000065e5', s));
+	BOOST_CHECK (s.length() == 3 && s[0] == '\xe6' && s[1] == '\x97' && s[2] == '\xa5');
 }
 
 
